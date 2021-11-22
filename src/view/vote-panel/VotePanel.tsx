b07b7d50eaea0614dce35594a51c27907d6716ee
Balazs Marton <balazs.marton@prezi.com>
2021-11-23 00:04:22 +0100

    style: basic formatting for votes

--- src/view/vote-panel/VotePanel.tsx
+++ src/view/vote-panel/VotePanel.tsx
@@ -10,9 +10,15 @@ const VotePanel = (props: VotePanelProps) => {
 
   const [voteCast, setVoteCast] = useState(props.voteCast);
 
+  const handleClick = (vote: Vote) => {
+    setVoteCast(vote);
+  }
+  
   return (
     <td>
-     {Object.values(Vote).map(vote => <span className = {vote === voteCast ? 'selected-vote' : 'non-selected-vote'}>{vote}</span>)}
+      {Object.values(Vote).map(
+        vote => <span className = {vote === voteCast ? 'selected-vote' : 'non-selected-vote'} onClick={() => handleClick(vote)}>{vote}</span>
+      )}
     </td>
   );
 }
