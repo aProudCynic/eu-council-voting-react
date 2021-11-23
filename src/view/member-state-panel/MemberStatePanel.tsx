8a5bdcbcb2554aa6f92280f6b9a166281f11d4e6
Balazs Marton <balazs.marton@prezi.com>
2021-11-23 09:00:50 +0100

    refact: eliminate state from vote component, propagate to member state

--- src/view/member-state-panel/MemberStatePanel.tsx
+++ src/view/member-state-panel/MemberStatePanel.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { MemberState } from '../../model/member-state';
+import { Vote } from '../../model/vote';
 import VotePanel from '../vote-panel/VotePanel';
 
 interface MemberStatePanelProps {
@@ -10,11 +11,16 @@ const MemberStatePanel = (props: MemberStatePanelProps) => {
 
   const [memberState, setMemberState] = useState(props.memberState);
 
+  const castVoteForMemberState = (vote: Vote) => {
+    const updatedMemberState = {...memberState, vote: vote};
+    setMemberState(updatedMemberState);
+  }
+
   return (
     <tr>
       <td>{memberState.name}</td>
       <td>{memberState.population}</td>
-      <VotePanel voteCast={memberState.vote}/>
+      <VotePanel voteCast={memberState.vote} voteCastingHandler={castVoteForMemberState}/>
     </tr>
   );
 }
--- src/view/vote-panel/VotePanel.tsx
+++ src/view/vote-panel/VotePanel.tsx
@@ -4,20 +4,19 @@ import './VotePanel.css';
 
 interface VotePanelProps {
   voteCast: Vote;
+  voteCastingHandler: Function;
 }
 
 const VotePanel = (props: VotePanelProps) => {
 
-  const [voteCast, setVoteCast] = useState(props.voteCast);
-
   const handleClick = (vote: Vote) => {
-    setVoteCast(vote);
+    props.voteCastingHandler(vote);
   }
   
   return (
     <td>
       {Object.values(Vote).map(
-        vote => <span className = {vote === voteCast ? 'selected-vote' : 'non-selected-vote'} onClick={() => handleClick(vote)}>{vote}</span>
+        vote => <span className = {vote === props.voteCast ? 'selected-vote' : 'non-selected-vote'} onClick={() => handleClick(vote)}>{vote}</span>
       )}
     </td>
   );
