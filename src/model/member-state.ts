a0ba1bef84fd4fd2f3ee5479a08c5bfd710c7fa0
Balazs Marton <balazs.marton@prezi.com>
2021-11-23 11:37:28 +0100

    fix: ditch name immutability as it interfered with changing state

--- src/model/member-state.ts
+++ src/model/member-state.ts
@@ -1,17 +1,13 @@
 import { Vote } from "./vote";
 
 export class MemberState {
-    private _name: string;
+    public name: string;
     public population: number;
     public vote: Vote;
 
     constructor(name: string, population: number) {
-        this._name = name;
+        this.name = name;
         this.population = population;
         this.vote = Vote.DID_NOT_VOTE;
-    }
-
-    public get name() {
-        return this._name;
     }
 }
