8d9eb26539bb69e55ecb223ecd3ac6da22348d92
Balazs Marton <balazs.marton@prezi.com>
2021-11-28 20:58:48 +0100

    chore: clean up state from vote widget

--- src/components/council-vote-widget/CouncilVoteWidget.tsx
+++ src/components/council-vote-widget/CouncilVoteWidget.tsx
@@ -1,9 +1,4 @@
-import React, { useState } from 'react';
-import { MemberState } from '../../model/member-state';
-import { Vote } from '../../model/vote';
-import { MemberStatesConstantLoader } from '../../service/member-states-constant-loader';
-import { MemberStatesLoader } from '../../service/member-states-loader';
-import VoteContext from '../../store/vote-context';
+import React from 'react';
 import MassVotingPanel from '../mass-voting-panel/MassVotingPanel';
 import MemberStatesPanel from '../member-states-panel/MemberStatesPanel';
 import VoteResultBoard from '../vote-result-board/VoteResultBoard';
@@ -11,20 +6,8 @@ import VoteSummaryBoard from '../vote-summary-board/VoteSummaryBoard';
 import './CouncilVoteWidget.css';
 
 const CouncilVoteWidget = () => {
-
-    const memberStatesLoader: MemberStatesLoader = new MemberStatesConstantLoader();
-
-    const [memberStates, setMemberStates] = useState(memberStatesLoader.loadMemberStates());
-
-    const voteCastingHandler = (vote: Vote, memberStateVoting: MemberState) => {
-        const newMemberStateData = { ...memberStateVoting, vote: vote };
-        setMemberStates(previousState => previousState.map(
-            memberState => memberState.name === memberStateVoting.name ? newMemberStateData : memberState
-        ));
-    }
-
+   
     return (
-        <VoteContext.Provider value={{ memberStates: memberStates, castVote: voteCastingHandler }}>
             <div className='widget-container'>
                 <div>
                     <MassVotingPanel />
@@ -35,7 +18,6 @@ const CouncilVoteWidget = () => {
                     <VoteResultBoard />
                 </div>
             </div>
-        </VoteContext.Provider>
     );
 }
 
