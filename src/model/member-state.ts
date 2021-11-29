b28f395eea341a200e09b7975c0457d53ffcc67d
Balazs Marton <balazs.marton@prezi.com>
2021-11-29 22:07:04 +0100

    feat: add id field to prep query param processing

--- src/model/member-state.ts
+++ src/model/member-state.ts
@@ -1,15 +1,21 @@
 import { Vote } from "./vote";
 
 export class MemberState {
+    private _id: string;
     public name: string;
     public population: number;
     public vote: Vote;
     public eurozoneMember: boolean;
 
-    constructor(name: string, population: number, eurozoneMember = false) {
+    constructor(id: string, name: string, population: number, eurozoneMember = false) {
+        this._id = id;
         this.name = name;
         this.population = population;
         this.vote = Vote.DID_NOT_VOTE;
         this.eurozoneMember = eurozoneMember;
+    }
+
+    get id() {
+        return this._id;
     }
 }
